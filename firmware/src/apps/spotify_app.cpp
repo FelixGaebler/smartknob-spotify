b513f5d81f355ed8e0def174d2271772fb1ae0f6
Felix Gaebler <felix@gaebler.dev>
2024-10-04 14:26:39 +0200

    Checkpoint code removal #3

--- firmware/src/apps/spotify_app.cpp
+++ firmware/src/apps/spotify_app.cpp
@@ -1,11 +1,68 @@
 #include "spotify_app.h"
 
-SpotifyApp::SpotifyApp(SemaphoreHandle_t mutex) : Apps(mutex)
+SpotifyApp::SpotifyApp(SemaphoreHandle_t mutex) : screen_mutex_(mutex)
 {
+    app_mutex_ = xSemaphoreCreateMutex();
     clear();
 
     StopwatchApp *stopwatch_app = new StopwatchApp(screen_mutex_, "stopwatch");
     add(0, stopwatch_app);
 
     setActive(0);
+}
+
+void SpotifyApp::add(uint8_t id, App *app)
+{
+    SemaphoreGuard lock(app_mutex_);
+    apps.insert(std::make_pair(id, app));
+}
+
+void SpotifyApp::clear()
+{
+    SemaphoreGuard lock(app_mutex_);
+    apps.clear();
+}
+
+EntityStateUpdate SpotifyApp::update(AppState state)
+{
+    SemaphoreGuard lock(app_mutex_);
+    EntityStateUpdate new_state_update;
+
+    new_state_update = active_app->updateStateFromKnob(state.motor_state);
+    active_app->updateStateFromSystem(state);
+    return new_state_update;
+}
+
+void SpotifyApp::render()
+{
+    active_app->render();
+}
+
+void SpotifyApp::setActive(int8_t id)
+{
+    SemaphoreGuard lock(app_mutex_);
+    active_id = id;
+    active_app = apps[id];
+    render();
+}
+
+void SpotifyApp::setMotorNotifier(MotorNotifier *motor_notifier)
+{
+    this->motor_notifier = motor_notifier;
+}
+
+void SpotifyApp::triggerMotorConfigUpdate()
+{
+    motor_notifier->requestUpdate(active_app->getMotorConfig());
+}
+
+void SpotifyApp::handleNavigationEvent(NavigationEvent event)
+{
+    setActive(0);
+    motor_notifier->requestUpdate(active_app->getMotorConfig());
+}
+
+void SpotifyApp::setOSConfigNotifier(OSConfigNotifier *os_config_notifier)
+{
+    os_config_notifier_ = os_config_notifier;
 }
--- firmware/src/apps/spotify_app.h
+++ firmware/src/apps/spotify_app.h
@@ -1,8 +1,37 @@
 #pragma once
-#include "apps.h"
 
-class SpotifyApp : public Apps
+#include <map>
+#include "../app_config.h"
+#include "../notify/motor_notifier/motor_notifier.h"
+#include "../navigation/navigation.h"
+#include "./notify/os_config_notifier/os_config_notifier.h"
+#include "apps/stopwatch/stopwatch.h"
+
+class SpotifyApp
 {
 public:
     SpotifyApp(SemaphoreHandle_t mutex);
+
+    void add(uint8_t id, App *app);
+    void clear();
+    EntityStateUpdate update(AppState state);
+    void render();
+    void setActive(int8_t id);
+
+    void setMotorNotifier(MotorNotifier *motor_notifier);
+    void setOSConfigNotifier(OSConfigNotifier *os_config_notifier);
+    void triggerMotorConfigUpdate();
+    void handleNavigationEvent(NavigationEvent event);
+
+private:
+    SemaphoreHandle_t screen_mutex_;
+    SemaphoreHandle_t app_mutex_;
+    std::map<uint8_t, std::shared_ptr<App>> apps;
+
+    int8_t active_id = 0;
+    std::shared_ptr<App> active_app = nullptr;
+
+    PB_SmartKnobConfig root_level_motor_config;
+    MotorNotifier *motor_notifier;
+    OSConfigNotifier *os_config_notifier_;
 };
